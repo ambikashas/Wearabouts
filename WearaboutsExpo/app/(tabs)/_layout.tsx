66a824b9c1c9803faec3e2f39d94af5bf7e148e5
Amy Lee <146399522+leeaj23@users.noreply.github.com>
2025-10-28 13:47:31 -0700

    Outfit Generation Screen (#4)
    * removing failing dependency
    
    * updating dependencies
    
    * basic components and generate screen done
    
    * making sure dependencies work
    
    * Adding styling to the generate pages

--- WearaboutsExpo/app/(tabs)/_layout.tsx
+++ WearaboutsExpo/app/(tabs)/_layout.tsx
@@ -44,6 +44,12 @@ export default function TabLayout() {
           href: null, // This hides the tab from the tab bar
         }}
       />
+      <Tabs.Screen
+        name="generated-outfit"
+        options={{
+          href: null, // This hides the tab from the tab bar
+        }}
+      />
     </Tabs>
   );
 }
