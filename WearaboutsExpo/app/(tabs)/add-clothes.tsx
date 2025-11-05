616e831d347c899113af7f8343d400b07b87d164
nina-uchtmann <ninaucht@uw.edu>
2025-11-04 16:08:02 -0800

    Fix confetti on add-clothes page (#21)
    * Add user manual
    
    * Update user manual
    
    * package json updates for jest
    
    * package json updates for jest (2)
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * Update package-lock.json
    
    * Add home screen tests from main
    
    * Delete extra files
    
    * Delete duplicate files in WearaboutsExpo
    
    * bg color white during uploading... text
    
    * Adjust opacity on tap for add-clothes buttons and increase size of tap to upload button
    
    * Add shadow to clickable buttons on add-clothes
    
    * Add shadow to clickable buttons on add-clothes

--- WearaboutsExpo/app/(tabs)/add-clothes.tsx
+++ WearaboutsExpo/app/(tabs)/add-clothes.tsx
@@ -67,7 +67,7 @@ export default function AddClothesScreen() {
 
   if (isLoading) {
     return (
-      <View className="flex-1 bg-[#FFE4E1] justify-center items-center p-4">
+      <View className="flex-1 justify-center items-center p-4">
         <ActivityIndicator size="large" color="#FF69B4" />
         <Text className="mt-2 text-base text-brandPink">Uploading...</Text>
       </View>
@@ -78,7 +78,8 @@ export default function AddClothesScreen() {
     <View className="flex-1 p-4">
       {/* Upload Area */}
       <TouchableOpacity
-        className="w-full mb-4 rounded-2xl bg-brandPink justify-center items-center p-5"
+        activeOpacity={0.7}
+        className="w-full mb-4 rounded-2xl bg-brandPink justify-center items-center p-8 shadow-sm shadow-black/10"
         onPress={pickImages}
       >
         <Text className="text-base text-white">Tap to upload images</Text>
@@ -107,6 +108,7 @@ export default function AddClothesScreen() {
             <View key={index} className="relative p-1 w-1/4">
               <Image source={{ uri }} className="aspect-square rounded-lg" />
               <TouchableOpacity
+                activeOpacity={0.7}
                 className="absolute top-1 right-1 bg-[#fd6cb5] w-6 h-6 rounded-full justify-center items-center"
                 onPress={() => deleteImage(uri)}
               >
@@ -120,10 +122,11 @@ export default function AddClothesScreen() {
       {/* Upload Button */}
       <View className="items-center pt-4">
         <TouchableOpacity
+          activeOpacity={0.7}
           className={`w-[150px] py-3 rounded-lg items-center ${
             imageUris.length === 0 || isLoading
-              ? "bg-brandPink"
-              : "bg-[#FF69B4]"
+              ? "bg-brandPink opacity-60"
+              : "bg-brandPink shadow-sm shadow-black/10"
           }`}
           disabled={imageUris.length === 0 || isLoading}
           onPress={handleUpload}
