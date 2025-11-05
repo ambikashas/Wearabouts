2cba4c7d41b19d31aba17613ecc152295e919811
lauw <laura.paraboschi@hotmail.com>
2025-11-04 20:22:22 -0800

    Fix transformIgnorePatterns regex

--- WearaboutsExpo/jest.config.js
+++ WearaboutsExpo/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: "jest-expo",
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transformIgnorePatterns: [
-    "node_modules/(?!(jest-)?@?react-native|expo|@expo|@unimodules)",
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg)",
   ],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
